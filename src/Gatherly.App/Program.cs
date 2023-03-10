302737e5fd6d60295f5389d14a1356a1b7c7601b
Dev <Dev@DEVBALA>
2023-03-10 07:55:02 -0400

    Add project files.

---
+++ src/Gatherly.App/Program.cs
@@ -0,0 +1,26 @@
+using Gatherly.App.Configuration;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .InstallServices(
+        builder.Configuration,
+        typeof(IServiceInstaller).Assembly);
+
+WebApplication app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ src/Gatherly.Persistence/Specifications/SpecificationEvaluator.cs
@@ -0,0 +1,50 @@
+﻿using Gatherly.Domain.Primitives;
+using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace Gatherly.Persistence.Specifications;
+
+public static class SpecificationEvaluator
+{
+    public static IQueryable<TEntity> GetQuery<TEntity>(
+        IQueryable<TEntity> inputQueryable,
+        Specification<TEntity> specification)
+        where TEntity : Entity
+    {
+        IQueryable<TEntity> queryable = inputQueryable;
+
+        if (specification.Criteria is not null)
+        {
+            queryable = queryable.Where(specification.Criteria);
+        }
+
+        specification.IncludeExpressions.Aggregate(
+            queryable,
+            (current, includeExpression) =>
+                current.Include(includeExpression));
+
+        if (specification.OrderByExpression is not null)
+        {
+            queryable = queryable.OrderBy(specification.OrderByExpression);
+        }
+        else if (specification.OrderByDescendingExpression is not null)
+        {
+            queryable = queryable.OrderByDescending(
+                specification.OrderByDescendingExpression);
+        }
+        if (specification.GroupByExpression != null)
+        {
+            queryable = queryable.GroupBy(specification.GroupByExpression).SelectMany(x => x);
+        }
+        if (specification.IsSplitQuery)
+        {
+            queryable = queryable.AsSplitQuery();
+        }
+        if (specification.IsPagingEnabled)
+        {
+            queryable = queryable.Skip(specification.Skip)
+                         .Take(specification.Take);
+        }
+        return queryable;
+    }
+}
